ae224680cb9e79fc82836600377de32104991ac9
Ignazio Bovo <ignazio@jsgenesis.com>
2023-11-21 14:18:51 +0100

    fix: :bug: misc errors from configuration

--- Makefile
+++ Makefile
@@ -35,13 +35,19 @@ prepare: install codegen build
 
 up-squid:
 	@docker network create joystream_default || true
-	@docker-compose up -d
+	@docker-compose up -d orion_db
+	@docker-compose up -d orion_processor
+	@docker-compose up -d orion_graphql-server
+	@docker-compose up -d orion_auth-api
+
+up-elk:
+	./scripts/start-elasticsearch.sh
 
 up-archive:
 	@docker network create joystream_default || true
 	@docker-compose -f archive/docker-compose.yml up -d
 
-up: up-archive up-squid
+up: up-archive up-squid up-elk
 
 down-squid:
 	@docker-compose down -v
@@ -49,6 +55,6 @@ down-squid:
 down-archive:
 	@docker-compose -f archive/docker-compose.yml down -v
 
-down: down-squid down-archive
+down: down-squid down-archive down-elasticsearch
 
-.PHONY: build serve process migrate codegen typegen prepare up-squid up-archive up down-squid down-archive down
+.PHONY: build serve process migrate codegen typegen prepare up-squid up-archive up-elasticsearch up down-squid down-archive down-elasticsearch down 
---
+++ scripts/start-elasticsearch.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+cd $SCRIPT_PATH
+
+set -a
+if [ -f ../.env ]; then
+    . ../.env
+fi
+set +a
+
+ELASTIC_USERNAME=${ELASTIC_USERNAME:="elastic"}
+ELASTIC_PASSWORD=${ELASTIC_PASSWORD:="password"}
+
+## Run docker-compose to start elasticsearch container
+docker-compose -f ../docker-compose.yml up -d elasticsearch
+
+printf 'Waiting for Elasticsearch...\n'
+
+sleep 10
+
+# Generate & Export a random 32-character encryption key as an environment variable
+# Ref: https://www.elastic.co/guide/en/kibana/current/xpack-security-secure-saved-objects.html#xpack-security-secure-saved-objects
+export XPACK_ENCRYPTEDSAVEDOBJECTS_ENCRYPTIONKEY=$(openssl rand -base64 24)
+
+# Generate the service token
+response=$(curl -f -s -X POST -u "${ELASTIC_USERNAME}":"${ELASTIC_PASSWORD}" "http://localhost:9200/_security/service/elastic/kibana/credential/token/my_kibana_token" -H 'Content-Type: application/json')
+
+# Extract & export the token from the response
+export ELASTICSEARCH_SERVICEACCOUNTTOKEN=$(echo $response | jq -r '.token.value')
+
+# ## setup filebeat for orion_db
+docker-compose -f ../docker-compose.yml up -d filebeat
+
+## Run docker-compose to start kibana container
+docker-compose -f ../docker-compose.yml up -d kibana
